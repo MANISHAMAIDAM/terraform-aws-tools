d1957f0d84cf3590ac0df67cb03ffd049dec7542
manishamaidam <maidammanisha.gmail.com>
2025-05-15 13:06:47 +0530

    cicd

--- cicd/data.tf
+++ cicd/data.tf
@@ -17,4 +17,28 @@ data "aws_ami" "ami_info" {
         name   = "virtualization-type"
         values = ["hvm"]
     }
+
+    
+}
+
+
+data "aws_ami" "nexus_ami_info" {
+
+    most_recent = true
+    owners = ["679593333241"]
+
+    filter {
+        name   = "name"
+        values = ["SolveDevOps-Nexus-Server-Ubuntu24.04-20250222-*"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
