6b40ff65ef50a29fe6ef8ed98142d2633128dfe2
manishamaidam <maidammanisha.gmail.com>
2025-05-15 12:59:56 +0530

    cicd

--- cicd/main.tf
+++ cicd/main.tf
@@ -29,6 +29,39 @@ module "jenkins_agent" {
   }
 }
 
+
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKhxR21nQSL96I5PXq+GcCgenKZdRWYI7XjVn5UO4tIX manis@LENOVO"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0695801005722ab4c"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-0e5547f3dca978568"
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -51,6 +84,16 @@ module "records" {
       ttl     = 1
       records = [
         module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+    },
+      {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
       ]
       allow_overwrite = true
     }
